0c5d45761cad11edb1d93ca9b2b65753749b9c87
Alex <a_vydrin@hotmail.com>
2016-04-20 21:01:46 +0300

    print added

---
+++ Smart Spender/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Smart Spender
+//
+//  Created by Alex on 20.04.16.
+//  Copyright © 2016 AppStory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Hello")
+    }
+
+    
+
+
+}
+
